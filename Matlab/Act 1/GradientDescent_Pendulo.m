20177fd651d033d43ebaf02e09c44f163543f6e2
LeonardoNavaC <A01750595@itesm.mx>
2022-10-12 14:12:44 -0500

    Activity 1 Finished V 0.1

--- Matlab/Act 1/GradientDescent_Pendulo.m
+++ Matlab/Act 1/GradientDescent_Pendulo.m
@@ -14,25 +14,23 @@ for i = 1: 1500/2
    datos_salida_theta_2d(i+1) = theta_2d(i*2);
 end  
 %%
-syms w1 w2 w3 w4 w5 w6 w7 w8 w9 w10 w11 w12 w13 w14 w15 w16 w17 w18 w19 w20
-syms w21 w22 w23 w24 w25 w26 w27 w28 w29 w30 w31 w32 w33 w34 w35 w36 w37 w38 w39 w40
-syms w41 w42 w43 
-syms b1 b2 b3 b4 b5 b6 b7 b8 b9 b10 b11 b12
+syms W1 W2 W3 W4 W5 W6 W7 W8 W9 W10 W11 W12 W13 W14 W15 W16 W17 W18 W19 W20 W21 W22 W23 W24 W25 W26 W27 W28 W29 W30 W31 W32 W33 W34 W35 W36 W37 W38 W39 W40 W41 W42 W43 
+syms B1 B2 B3 B4 B5 B6 B7 B8 B9 B10 B11 B12
 syms theta thetad
 
-n1=w1*theta+w2*thetad + b1;         n2=w3*theta+w4*thetad + b2;
-n3=w5*theta+w6*thetad + b3;         n4=w7*theta+w8*thetad + b4;
-n5=w9*theta+w10*thetad + b5;        n6=w11*theta+w12*thetad + b6;
-n7=w13*theta+w14*thetad + b7;       n8=w15*theta+w16*thetad + b8;
+n1=W1*theta+W2*thetad + B1;         n2=W3*theta+W4*thetad + B2;
+n3=W5*theta+W6*thetad + B3;         n4=W7*theta+W8*thetad + B4;
+n5=W9*theta+W10*thetad + B5;        n6=W11*theta+W12*thetad + B6;
+n7=W13*theta+W14*thetad + B7;       n8=W15*theta+W16*thetad + B8;
 
 sn1 = 1/(1+exp(-(n1))); sn2 = 1/(1+exp(-(n2))); sn3 = 1/(1+exp(-(n3))); sn4 = 1/(1+exp(-(n4)));  
 sn5 = 1/(1+exp(-(n5))); sn6 = 1/(1+exp(-(n6))); sn7 = 1/(1+exp(-(n7))); sn8 = 1/(1+exp(-(n8))); 
 
-beta1 = w20*sn1 + w21*sn2 + w22*sn3 + w23*sn4 + w24*sn5 + w25*sn6 +w26*sn7 + w27*sn8 +b10;
-beta2 = w28*sn1 + w29*sn2 + w30*sn3 + w31*sn4 + w32*sn5 + w33*sn6 +w34*sn7 + w35*sn8 +b11;
-beta3 = w36*sn1 + w37*sn2 + w38*sn3 + w39*sn4 + w40*sn5 + w41*sn6 +w42*sn7 + w43*sn8 +b12;
+Beta1 = W20*sn1 + W21*sn2 + W22*sn3 + W23*sn4 + W24*sn5 + W25*sn6 +W26*sn7 + W27*sn8 +B10;
+Beta2 = W28*sn1 + W29*sn2 + W30*sn3 + W31*sn4 + W32*sn5 + W33*sn6 +W34*sn7 + W35*sn8 +B11;
+Beta3 = W36*sn1 + W37*sn2 + W38*sn3 + W39*sn4 + W40*sn5 + W41*sn6 +W42*sn7 + W43*sn8 +B12;
 
-theta_2d_salida = ((w17/(1+exp(-(beta1))))+ (w18/(1+exp(-(beta2)))) + (w19/(1+exp(-(beta3)))) + b9);
+theta_2d_salida = ((W17/(1+exp(-(Beta1))))+ (W18/(1+exp(-(Beta2)))) + (W19/(1+exp(-(Beta3)))) + B9);
 %%
 fo = 0;
 for i = 1:21
@@ -42,56 +40,56 @@ end
 disp(fo)
 %%
 alfa = 0.001; e = 0.01; n = 1;
-gradiente = [diff(fo, w1),diff(fo, w2), diff(fo, w3), diff(fo, w4), diff(fo, w5), diff(fo, w6)...
-            diff(fo, w7),diff(fo, w8), diff(fo, w9), diff(fo, w10), diff(fo, w11), diff(fo, w12)...
-            diff(fo, w13),diff(fo, w14), diff(fo, w15), diff(fo, w16), diff(fo, w17), diff(fo, w18)...
-            diff(fo, w19),diff(fo, w20), diff(fo, w21), diff(fo, w22), diff(fo, w23), diff(fo, w24)...
-            diff(fo, w25),diff(fo, w26), diff(fo, w27), diff(fo, w28), diff(fo, w29), diff(fo, w30)...
-            diff(fo, w31),diff(fo, w32), diff(fo, w33), diff(fo, w34), diff(fo, w35), diff(fo, w36)...
-            diff(fo, w37),diff(fo, w38), diff(fo, w39), diff(fo, w40), diff(fo, w41), diff(fo, w42)...
-            diff(fo, w43), diff(fo, b1), diff(fo, b2), diff(fo, b3), diff(fo, b4), diff(fo, b5), diff(fo, b6)...
-            diff(fo, b7), diff(fo, b8), diff(fo, b9), diff(fo, b10), diff(fo, b11), diff(fo, b12)];
+gradiente = [diff(fo, W1),diff(fo, W2), diff(fo, W3), diff(fo, W4), diff(fo, W5), diff(fo, W6)...
+            diff(fo, W7),diff(fo, W8), diff(fo, W9), diff(fo, W10), diff(fo, W11), diff(fo, W12)...
+            diff(fo, W13),diff(fo, W14), diff(fo, W15), diff(fo, W16), diff(fo, W17), diff(fo, W18)...
+            diff(fo, W19),diff(fo, W20), diff(fo, W21), diff(fo, W22), diff(fo, W23), diff(fo, W24)...
+            diff(fo, W25),diff(fo, W26), diff(fo, W27), diff(fo, W28), diff(fo, W29), diff(fo, W30)...
+            diff(fo, W31),diff(fo, W32), diff(fo, W33), diff(fo, W34), diff(fo, W35), diff(fo, W36)...
+            diff(fo, W37),diff(fo, W38), diff(fo, W39), diff(fo, W40), diff(fo, W41), diff(fo, W42)...
+            diff(fo, W43), diff(fo, B1), diff(fo, B2), diff(fo, B3), diff(fo, B4), diff(fo, B5), diff(fo, B6)...
+            diff(fo, B7), diff(fo, B8), diff(fo, B9), diff(fo, B10), diff(fo, B11), diff(fo, B12)];
 %%
-array_w_b = zeros(1,55);
+array_W_B = zeros(1,55);
 
 
-w1 = array_w_b(1);    w2 = array_w_b(2);    w3 = array_w_b(3);         w4 = array_w_b(4); 
-w5 = array_w_b(5);    w6 = array_w_b(6);    w7 = array_w_b(7);         w8 = array_w_b(8);
-w9 = array_w_b(9);    w10 = array_w_b(10);  w11 = array_w_b(11);       w12 = array_w_b(12); 
-w13 = array_w_b(13);  w14 = array_w_b(14);  w15 = array_w_b(15);       w16 = array_w_b(16);
-w17 = array_w_b(17);  w18 = array_w_b(18);  w19 = array_w_b(19);       w20 = array_w_b(20); 
-w21 = array_w_b(21);  w22 = array_w_b(22);  w23 = array_w_b(23);       w24 = array_w_b(24);
-w25 = array_w_b(25);  w26 = array_w_b(26);  w27 = array_w_b(27);       w28 = array_w_b(28); 
-w29 = array_w_b(29);  w30 = array_w_b(30);  w31 = array_w_b(31);
-w32 = array_w_b(32);  w33 = array_w_b(33);
-w34 = array_w_b(34);  w35 = array_w_b(35);  w36 = array_w_b(36);       w37 = array_w_b(37); 
-w38 = array_w_b(38);  w39 = array_w_b(39);  w40 = array_w_b(40);       w41 = array_w_b(41);
-w42 = array_w_b(42);  w43 = array_w_b(43);  b1 = array_w_b(44);        b2 = array_w_b(45); 
-b3 = array_w_b(46);   b4 = array_w_b(47);   b5 = array_w_b(48);        b6 = array_w_b(49);
-b7 = array_w_b(50);   b8 = array_w_b(51);   b9 = array_w_b(52);        b10 = array_w_b(53);
-b11 = array_w_b(54);  b12= array_w_b(55);
+W1 = array_W_B(1);    W2 = array_W_B(2);    W3 = array_W_B(3);         W4 = array_W_B(4); 
+W5 = array_W_B(5);    W6 = array_W_B(6);    W7 = array_W_B(7);         W8 = array_W_B(8);
+W9 = array_W_B(9);    W10 = array_W_B(10);  W11 = array_W_B(11);       W12 = array_W_B(12); 
+W13 = array_W_B(13);  W14 = array_W_B(14);  W15 = array_W_B(15);       W16 = array_W_B(16);
+W17 = array_W_B(17);  W18 = array_W_B(18);  W19 = array_W_B(19);       W20 = array_W_B(20); 
+W21 = array_W_B(21);  W22 = array_W_B(22);  W23 = array_W_B(23);       W24 = array_W_B(24);
+W25 = array_W_B(25);  W26 = array_W_B(26);  W27 = array_W_B(27);       W28 = array_W_B(28); 
+W29 = array_W_B(29);  W30 = array_W_B(30);  W31 = array_W_B(31);
+W32 = array_W_B(32);  W33 = array_W_B(33);
+W34 = array_W_B(34);  W35 = array_W_B(35);  W36 = array_W_B(36);       W37 = array_W_B(37); 
+W38 = array_W_B(38);  W39 = array_W_B(39);  W40 = array_W_B(40);       W41 = array_W_B(41);
+W42 = array_W_B(42);  W43 = array_W_B(43);  B1 = array_W_B(44);        B2 = array_W_B(45); 
+B3 = array_W_B(46);   B4 = array_W_B(47);   B5 = array_W_B(48);        B6 = array_W_B(49);
+B7 = array_W_B(50);   B8 = array_W_B(51);   B9 = array_W_B(52);        B10 = array_W_B(53);
+B11 = array_W_B(54);  B12= array_W_B(55);
 
 gradiente_numerico = vpa(subs(gradiente));
 norma = norm(gradiente_numerico);
 %%
 while norma > e
-    array_w_b = array_w_b - alfa * gradiente_numerico;
-    w1 = array_w_b(1);    w2 = array_w_b(2);    w3 = array_w_b(3);         w4 = array_w_b(4); 
-    w5 = array_w_b(5);    w6 = array_w_b(6);    w7 = array_w_b(7);         w8 = array_w_b(8);
-    w9 = array_w_b(9);    w10 = array_w_b(10);  w11 = array_w_b(11);       w12 = array_w_b(12);     
-    w13 = array_w_b(13);  w14 = array_w_b(14);  w15 = array_w_b(15);       w16 = array_w_b(16);
-    w17 = array_w_b(17);  w18 = array_w_b(18);  w19 = array_w_b(19);       w20 = array_w_b(20); 
-    w21 = array_w_b(21);  w22 = array_w_b(22);  w23 = array_w_b(23);       w24 = array_w_b(24);
-    w25 = array_w_b(25);  w26 = array_w_b(26);  w27 = array_w_b(27);       w28 = array_w_b(28); 
-    w29 = array_w_b(29);  w30 = array_w_b(31);  w32 = array_w_b(32);       w33 = array_w_b(33);
-    w34 = array_w_b(34);  w35 = array_w_b(35);  w36 = array_w_b(36);       w37 = array_w_b(37); 
-    w38 = array_w_b(38);  w39 = array_w_b(39);  w40 = array_w_b(40);       w41 = array_w_b(41);
-    w42 = array_w_b(42);  w43 = array_w_b(43);  b1 = array_w_b(44);        b2 = array_w_b(45); 
-    b3 = array_w_b(46);   b4 = array_w_b(47);   b5 = array_w_b(48);        b6 = array_w_b(49);
-    b7 = array_w_b(50);   b8 = array_w_b(51);   b9 = array_w_b(52);        b10 = array_w_b(53);
-    b11 = array_w_b(54);  b12= array_w_b(55);
+    array_W_B = array_W_B - alfa * gradiente_numerico;
+    W1 = array_W_B(1);    W2 = array_W_B(2);    W3 = array_W_B(3);         W4 = array_W_B(4); 
+    W5 = array_W_B(5);    W6 = array_W_B(6);    W7 = array_W_B(7);         W8 = array_W_B(8);
+    W9 = array_W_B(9);    W10 = array_W_B(10);  W11 = array_W_B(11);       W12 = array_W_B(12);     
+    W13 = array_W_B(13);  W14 = array_W_B(14);  W15 = array_W_B(15);       W16 = array_W_B(16);
+    W17 = array_W_B(17);  W18 = array_W_B(18);  W19 = array_W_B(19);       W20 = array_W_B(20); 
+    W21 = array_W_B(21);  W22 = array_W_B(22);  W23 = array_W_B(23);       W24 = array_W_B(24);
+    W25 = array_W_B(25);  W26 = array_W_B(26);  W27 = array_W_B(27);       W28 = array_W_B(28); 
+    W29 = array_W_B(29);  W30 = array_W_B(31);  W32 = array_W_B(32);       W33 = array_W_B(33);
+    W34 = array_W_B(34);  W35 = array_W_B(35);  W36 = array_W_B(36);       W37 = array_W_B(37); 
+    W38 = array_W_B(38);  W39 = array_W_B(39);  W40 = array_W_B(40);       W41 = array_W_B(41);
+    W42 = array_W_B(42);  W43 = array_W_B(43);  B1 = array_W_B(44);        B2 = array_W_B(45); 
+    B3 = array_W_B(46);   B4 = array_W_B(47);   B5 = array_W_B(48);        B6 = array_W_B(49);
+    B7 = array_W_B(50);   B8 = array_W_B(51);   B9 = array_W_B(52);        B10 = array_W_B(53);
+    B11 = array_W_B(54);  B12= array_W_B(55);
     gradiente_numerico = vpa(subs(gradiente));
     norma = norm(gradiente_numerico);
     
 end
-disp(array_w_b)
+disp(array_W_B)
